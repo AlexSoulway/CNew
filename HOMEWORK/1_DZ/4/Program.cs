384e6586da727a910633deec14a0de9053d16f36
Alex <soullovesthesun@gmail.com>
2022-07-12 13:45:46 +0300

    add Hw246

---
+++ HOMEWORK/1_DZ/4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введи 3 числа и мы тебе скажем какое из них больше");
+            int a, b, c, max;
+            Console.WriteLine("Введи первое число - ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введи второе число - ");
+            b = Convert.ToInt32(Console.ReadLine());
+              Console.WriteLine("Введи третье число - ");
+            c = Convert.ToInt32(Console.ReadLine());
+             max = a;
+           
+
+
+
+      
+if (b > a)
+    max = b;
+if (c > max)
+    max = c;
+             Console.WriteLine($"Самое большое число {max}");
