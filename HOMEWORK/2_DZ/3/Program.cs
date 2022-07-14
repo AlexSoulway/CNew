e033b0a20bf76afe64709d32ed54bfb583e95b09
Александр Соловьев <aleksandrsolovev@Aleksandrs-MacBook-Air.local>
2022-07-15 01:01:46 +0300

    add HOMEWORK DZ2_3

--- HOMEWORK/2_DZ/3/Program.cs
+++ HOMEWORK/2_DZ/3/Program.cs
@@ -2,3 +2,20 @@
 //645 -> 5
 //78 -> третьей цифры нет
 //32679 -> 6
+Console.WriteLine("Write a number: ");
+int n = int.Parse(Console.ReadLine());
+
+    if (n > 100 && n < 1000)
+    Console.WriteLine(n % 10);
+    else if (n < 100)
+    Console.WriteLine("нет третьей цифры");
+    else if (n > 1000 && n < 10000)
+    Console.WriteLine(n / 10 % 10);
+     else if (n > 10000 && n < 100000)
+    Console.WriteLine(n / 100 % 10);
+     else if (n > 100000 && n < 1000000)
+    Console.WriteLine(n / 1000 % 10);
+    
+    
+    
+    Console.WriteLine();
