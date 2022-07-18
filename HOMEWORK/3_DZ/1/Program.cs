01ccddb8d89dc6f9057dc2819d9dd74ed9bae00f
Александр Соловьев <aleksandrsolovev@Aleksandrs-MacBook-Air.local>
2022-07-18 17:59:59 +0300

    add Hw3_1

---
+++ HOMEWORK/3_DZ/1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Введите пятизначное число и мы скажем является ли оно палиондромом");
+int num = int.Parse(Console.ReadLine());
+
+int a = Convert.ToInt32(num / 10000);
+int b = Convert.ToInt32(num / 1000 % 10);
+int c = Convert.ToInt32(num / 10 % 10);
+int d = Convert.ToInt32(num % 10);
+if(a == d || b == c)
+{
+ Console.WriteLine("da");  
+}
+else
+{
+    Console.WriteLine("no");
+}
