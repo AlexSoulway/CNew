1d35d462b67351c55ca170016e7af38a4d248ad7
Александр Соловьев <aleksandrsolovev@Aleksandrs-MacBook-Air.local>
2022-08-04 16:14:58 +0300

    Hw4

---
+++ HOMEWORK/4_DZ/1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+int Method(int num)
+{
+    int AllSum = 1;
+    for (int i = 1; i <= num; i++)
+        AllSum = AllSum * i;
+    return AllSum;
+
+}
+Console.WriteLine(Method(4));
+
+
+
+// Console.ReadLine(a);
+// Console.ReadLine(b);
