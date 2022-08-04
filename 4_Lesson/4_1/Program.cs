3f16746501225a0fe00ccad69b917ac9d64da1c5
Александр Соловьев <aleksandrsolovev@Aleksandrs-MacBook-Air.local>
2022-08-04 14:45:48 +0300

    task 4_1

---
+++ 4_Lesson/4_1/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу которая на вход принимает число  и выдает количество цифр в числе
+void Length (int N) 
+{
+    string num = N.ToString();
+   Console.WriteLine(num.Length);
+}
+Length(5657);
