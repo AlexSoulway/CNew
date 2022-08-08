63d88162f07c009ac98e0091eaf1b958480a9e39
Александр Соловьев <aleksandrsolovev@Aleksandrs-MacBook-Air.local>
2022-08-08 13:24:57 +0300

    task1

--- HOMEWORK/4_DZ/1/Program.cs
+++ HOMEWORK/4_DZ/1/Program.cs
@@ -1,17 +1,23 @@
-﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-// 3, 5 -> 243 (3⁵)
-// 2, 4 -> 16
-int Method(int num)
-{
-    int AllSum = 1;
-    for (int i = 1; i <= num; i++)
-        AllSum = AllSum * i;
-    return AllSum;
+﻿// 1. Напишите цикл, который принимает на вход два числа (A и B)
+//    и возводит число A в натуральную степень B.
 
+
+int PowNum(int a, int b)
+{   
+    int n_pow = a;
+    
+    for (int i = 2; i <= b; i++)
+    {
+        n_pow *= a; 
+    }  
+    return n_pow;  
 }
-Console.WriteLine(Method(4));
 
+Console.WriteLine("Enter first number: ");
+int num_1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second number: ");
+int num_2 = int.Parse(Console.ReadLine());
 
+Console.WriteLine(PowNum(num_1, num_2));
 
-// Console.ReadLine(a);
-// Console.ReadLine(b);
+Console.WriteLine(PowNum(2, 4));
