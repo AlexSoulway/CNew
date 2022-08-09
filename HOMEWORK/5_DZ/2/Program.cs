8d89c2d6e0215e45d8fa2bc1dbebbfdc4feded5c
Александр Соловьев <aleksandrsolovev@Aleksandrs-MacBook-Air.local>
2022-08-09 21:50:01 +0300

    2

---
+++ HOMEWORK/5_DZ/2/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 2: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 26
+// [-4, -6, 89, 6] -> 0
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] RandArray(int size, int from, int to)
+{
+    int[] arr = new int[8];
+
+    for (int i = 0; i < 8; i++)
+    {
+        arr[i] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+int Num(int[] arr)
+{
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i = i + 2)
+    {
+        
+        {
+            count += arr[i];
+        }        
+    }
+    return count;
+}
+
+int[] arr_1 = RandArray(8, 10, 20);
+Print(arr_1);
+Console.WriteLine(Num(arr_1));
